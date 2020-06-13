9c634f3b4e8023f797fe3af669d0c6057819fc01
vojtyys <vojta.pail@seznam.cz>
2020-06-13 21:11:39 +0200

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_blog_posts'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
